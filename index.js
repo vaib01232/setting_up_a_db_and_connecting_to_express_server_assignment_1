31533b5ce405b5a2548884b3905f6efac6f9986b
vaib01232 <119183394+vaib01232@users.noreply.github.com>
2025-02-10 12:02:37 +0530

    Completed assignment 1

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const express = require('express');
-const { resolve } = require('path');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
-const port = 3010;
 
-app.use(express.static('static'));
+const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+const mongoURI = process.env.MONGO_URI;
+
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log('Connected to database');
+})
+.catch((error) => {
+  console.error('Error connecting to database:', error.message);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.get('/', (req, res) => {
+  res.send('Customer Management System Backend is Running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
